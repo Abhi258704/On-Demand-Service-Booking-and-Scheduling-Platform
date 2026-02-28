7364a2874243975fcdd054e7db7da51d2ff5ef67
Baby-Coder <abhishekdinesh357@gmail.com>
2026-02-28 18:36:28 +0530

    connected to db

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,14 +1,10 @@
-import express from "express";
+import dotenv from "dotenv"
+import connectDB from "./db/index.js";
 
-const app  = express();
 
-app.get("/", (req, res)=>{
-    res.send("Server is ready"); 
+
+dotenv.config({
+    path: './env'
 })
 
-const port = process.env.PORT || 3000;
-
-app.listen(port,  ()=>{
-    console.log(`Server at http://localhost:${port}`);
-    
-})
+connectDB()
