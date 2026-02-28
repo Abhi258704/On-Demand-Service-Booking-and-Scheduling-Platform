83392c7232f9106ca0847072e19e5ade74674ced
Baby-Coder <abhishekdinesh357@gmail.com>
2026-02-28 20:26:21 +0530

    utils and tokens

--- Backend/src/models/user.models.js
+++ Backend/src/models/user.models.js
@@ -1,4 +1,7 @@
 import mongoose from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+import { use } from "react";
 
 
 const userSchema = new mongoose.Schema({
@@ -27,9 +30,65 @@ const userSchema = new mongoose.Schema({
     isActive: {
         type: Boolean,
         default: true
+    },
+    pfp: {
+        type: String
+    },
+    refresgToken: {
+        type: String
+    },
+    fullName: {
+        type: String,
+        required: true,
+        trim: true,
+        index: true
     }
 
 }, { timestamps: true })
+
+
+
+userSchema.pre("save", async function (next) {
+    if(!this.isModified("password")) return next();
+
+    this.password = bcrypt.hash(this.password, 10)
+    next()
+})
+
+userSchema.methods.isPasswordCorrect = async function (password){
+    return await bcrypt.compare(password, this.password)
+}
+
+
+
+userSchema.methods.generateAccessToken = function (){
+    return jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            username: this.username,
+            fullName: this.fullName
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+
+
+userSchema.methods.generateRefreshToken = function (){
+    return jwt.sign(
+        {
+            _id: this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
+
 
 
 export const User = mongoose.model("User", userSchema)
--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,5 +1,6 @@
 import dotenv from "dotenv"
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 
 
@@ -8,3 +9,12 @@ dotenv.config({
 })
 
 connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is runnning at port: ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("MongoDB conn failed !!", err);
+    
+})
